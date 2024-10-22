67452e824f09f610c5ada7472b18e2e1ae82caec
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-10-22 13:37:36 +0530

    create a navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { animateScroll as scroll } from 'react-scroll';
+import './Navbar.css'; // Import the CSS file
+
+const Navbar = ({ toggle }) => {
+    const [scrollNav, setScrollNav] = useState(false);
+
+    const changeNav = () => {
+        if (window.scrollY >= 80) {
+            setScrollNav(true);
+        } else {
+            setScrollNav(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeNav);
+        return () => window.removeEventListener('scroll', changeNav);
+    }, []);
+
+    const toggleHome = () => {
+        scroll.scrollToTop();
+    };
+
+    return (
+        <nav className={`navbar ${scrollNav ? 'scroll-nav' : ''}`}>
+            <div className="navbar-container">
+                <a href="/" className="nav-logo" onClick={toggleHome}>FlexZone</a>
+                <ul className="nav-menu">
+                    <li className="nav-item">
+                        <a href="/" className="nav-links">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/men" className="nav-links">Men</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/women" className="nav-links">Women</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/why-us" className="nav-links">Why US</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/membership" className="nav-links-contact">Membership</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href='/testimonial' className="nav-links">Testimonial</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href='/contact-us' className="nav-links">Contact Us</a>
+                    </li>
+                </ul>
+                <div className="nav-btn">
+                    <a href="/login" className="nav-btn-link">Log In</a>
+                </div>
+                <div className="mobile-icon" onClick={toggle}>
+                    <FaBars />
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
